22d4911727e589da1955fa513404df9d16684aec
KirilZal <kirilzalov@gmail.com>
2024-04-04 20:00:42 +0200

    add

--- Part5/MyClass.cs
+++ Part5/MyClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace HomeWork11
 { 
@@ -7,6 +8,14 @@ namespace HomeWork11
         public static T FactoryMethod()
         {
             return default(T);
+
+        }
+    }
+    class Myclass2<T> where T: new()
+    {
+        public static T Fabryk()
+        {
+            return new T();
 
         }
     }
